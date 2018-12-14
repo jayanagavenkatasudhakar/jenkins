db8572319da51d721544c599b902022821c2ee47
jayanagavenkatasudhakar <jaya.sudhakar22@gmail.com>
2018-12-14 19:00:54 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -4,7 +4,7 @@ class Demo
     // Prints "Hello, World" to the terminal window. 
     public static void main(String args[]) 
     { 
-        System.out.println("Hello !!! jaya 11th Dec 2018\n Showing PIPELINE DEMO"); 
+        System.out.println("Hello !!! jaya 14th Dec 2018\n Showing 'Tsk1.2_pulling_source_code_from_github'"); 
 		System.out.println("Good BYe ..........Mr!!!!!");
     } 
 } 
