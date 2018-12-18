708fd1fad14796db0f8be3f2cfc6d1934cfec313
jayanagavenkatasudhakar <jaya.sudhakar22@gmail.com>
2018-12-18 17:35:11 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -4,7 +4,7 @@ class Demo
     // Prints "Hello, World" to the terminal window. 
     public static void main(String args[]) 
     { 
-        System.out.println("Hello !!! Hemanth todya date is 17th Dec 2018\n 'Showing  PIPELINE DEMO Completed############ '"); 
+        System.out.println("Hello !!! Jaya todya date is 18th Dec 2018\n 'Showing  FreeStyle job DEMO Completed############ '"); 
 		System.out.println("Good BYe ..........Mr!!!!!");
     } 
 } 
